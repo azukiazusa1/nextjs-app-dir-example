8d63bf5630f9365e4d47db091b7084fb475a7304
azukiazusa1 <cycleippei@gmail.com>
2023-01-15 13:39:55 +0900

    as footer

--- app/Footer.tsx
+++ app/Footer.tsx
@@ -2,7 +2,7 @@ import { Container, Box, Text } from "./common/components";
 
 export default function Footer() {
   return (
-    <Box bg="gray.50" color="gray.700">
+    <Box bg="gray.50" color="gray.700" as="footer">
       <Container maxW="5xl" py={4}>
         <Text as="small">© 2023 azukiazusa</Text>
       </Container>
